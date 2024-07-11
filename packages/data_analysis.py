ec8eb9b63e573a3d8ad66877fdc0c196a16c8aa6
vincenzorrei <vin.orrei@gmail.com>
2024-07-11 09:03:51 +0200

    improve standard_OHE

--- packages/data_analysis.py
+++ packages/data_analysis.py
@@ -36,9 +36,14 @@ def standard_OHE(df):
     ).columns.tolist()
     encoder = OneHotEncoder(sparse_output=False)
     one_hot_encoded = encoder.fit_transform(df[categorical_columns])
+
     one_hot_df = pd.DataFrame(
         one_hot_encoded, columns=encoder.get_feature_names_out(categorical_columns)
     )
-    df_encoded = pd.concat([df, one_hot_df], axis=1)
+
+    # Join the one-hot encoded columns to the original DataFrame
+    df_encoded = df.join(one_hot_df)
+
+    # Drop the original categorical columns (redundant information)
     df_encoded = df_encoded.drop(categorical_columns, axis=1)
     return df_encoded
